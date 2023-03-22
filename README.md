eac7e57b6750647c93ab35f47d9d324c02278cef
Tranloi2k <tranloi20001007@gmail.com>
2023-03-22 17:44:00 +0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+Link to the endpoint of elastic beanstalk deployment:
+
+URL: http://image-filter-starter-code-dev22222.us-east-1.elasticbeanstalk.com/
+
+URL with example image url: http://image-filter-starter-code-dev22222.us-east-1.elasticbeanstalk.com/filteredimage?image_url=https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jpg
---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+
+  app.get("/filteredimage",async(req,res)=>{
+    let { image_url } = req.query;
+
+    // 1. validate the image_url query
+    const isValideUrl = image_url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    // i found this regrex at https://www.makeuseof.com/regular-expressions-validate-url 
+    if(isValideUrl == null)
+      return res.status(400).send(`Inavlid url! Try again with valid url`);
+    else{
+      // 2.call filterImageFromURL(image_url) to filter the image
+      const filteredImage = await filterImageFromURL(image_url);
+      //  3. send the resulting file in the response
+      //  4. deletes any files on the server on finish of the response
+      return res.status(200).sendFile(filteredImage, ( )=>{deleteLocalFiles([filteredImage])});
+    }
+})
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
